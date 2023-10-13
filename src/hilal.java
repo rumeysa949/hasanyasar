d3193d8f834542719d471c9bc81dc2ec516cc21d
Rumeysa949 <aslanrumeysa08@gmail.com>
2023-10-13 13:22:52 -0400

    first commit

---
+++ src/hilal.java
@@ -0,0 +1,15 @@
+public class hilal {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+
+        /*
+        git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rumeysa949/hasanyasar.git
+git push -u origin main
+         */
+    }
+}
